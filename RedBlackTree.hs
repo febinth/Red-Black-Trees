93072c56cedb01b1ff259dba81ed9c363bbb29cf
FT10\febin <thfebin@gmail.com>
2024-05-23 15:12:32 +0100

    Added search in RBT

--- RedBlackTree.hs
+++ RedBlackTree.hs
@@ -1,4 +1,4 @@
-module RedBlack where
+module RedBlackTree where
 
 -- Definition of the Red-Black Tree data structure
 
@@ -11,45 +11,49 @@ data RBT a = LeafRB | NodeRB Color (RBT a) a (RBT a)
 
 -- Serching a key inside a red-black tree
 --   return True if the key is found, False otherwise
-
 searchRB :: Ord a => a -> RBT a -> Bool
+searchRB key LeafRB = False
+searchRB key (NodeRB _ leftRBT a rightRBT)
+  | key < a = searchRB key leftRBT -- If key is less than root, search left sub tree
+  | key > a = searchRB key rightRBT -- If key is greater than root, search right sub tree
+  | otherwise = True -- If key is equal to root, return True
 
 
 -- Minimum and maximum of red-black tree
 --   return Nothing if the tree is empty
 
-minRB :: RBT a -> Maybe a
+-- minRB :: RBT a -> Maybe a
 
-maxRB :: RBT a -> Maybe a
+-- maxRB :: RBT a -> Maybe a
   
 
--- Check if a tree satisfies the Binary Search Tree condition
---   (do not check other RBT conditions)
-isBST :: Ord a => RBT a -> Bool
+-- -- Check if a tree satisfies the Binary Search Tree condition
+-- --   (do not check other RBT conditions)
+-- isBST :: Ord a => RBT a -> Bool
 
 
--- Check the Black-balancing condition:
---     all paths have the same number of black nodes
+-- -- Check the Black-balancing condition:
+-- --     all paths have the same number of black nodes
 
-blackBalanced :: RBT a -> Bool
-
-
--- Black height of a black-balanced tree, -1 if not black-balanced
-
-blackHeight :: RBT a -> Int
+-- blackBalanced :: RBT a -> Bool
 
 
--- Check if all Red-Black Tree conditions are satisfied
-isRBT :: Ord a => RBT a -> Bool
+-- -- Black height of a black-balanced tree, -1 if not black-balanced
+
+-- blackHeight :: RBT a -> Int
 
 
--- Insert a new element in a RBT, preserving the RBT properties
+-- -- Check if all Red-Black Tree conditions are satisfied
+-- isRBT :: Ord a => RBT a -> Bool
 
-insertRB :: Ord a => a -> RBT a -> RBT a
+
+-- -- Insert a new element in a RBT, preserving the RBT properties
+
+-- insertRB :: Ord a => a -> RBT a -> RBT a
 
 
--- Delete an element from a RBT, preserving the RBT properties
+-- -- Delete an element from a RBT, preserving the RBT properties
 
-deleteRB :: Ord a => a -> RBT a -> RBT a
+-- deleteRB :: Ord a => a -> RBT a -> RBT a
 
 
